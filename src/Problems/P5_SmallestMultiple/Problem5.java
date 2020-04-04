0ee97b581e26eb8413826ff1b4db33cce9e5e635
ryan <RJSukdeo@gmail.com>
2020-04-04 12:27:27 -0400

    Added solution for problem 5.

---
+++ src/Problems/P5_SmallestMultiple/Problem5.java
@@ -0,0 +1,33 @@
+package Problems.P5_SmallestMultiple;
+
+//Question
+//2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+//
+//What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+public class Problem5 {
+
+    private static final int[] DIVISORS = new int[]{11, 12, 13, 14, 15, 16, 17, 18, 19, 20};;
+
+    // In order for the number to be divisible by two, it must be even, hence traversing over even numbers.
+    public static void main(String[] args) {
+        int counter = 2;
+        while(true) {
+            if (isDivisible(counter)) {
+                break;
+            }
+            counter += 2;
+        }
+        System.out.println("Smallest Divisor: " + counter);
+    }
+
+    private static boolean isDivisible(int input) {
+        for (int divisor : DIVISORS) {
+            if (input % divisor != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
